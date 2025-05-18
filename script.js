0fca206a98642b7664b526d1df70d38947add3c2
achmedradaev2008 <radaevachmed2@gmail.com>
2025-05-18 11:39:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let name1="gif"
+let age2="17"
+
+console.log(name1);
+
+
+let body = document.querySelector("body")
+let footer = document.querySelector("footer")
+let cflogo = document.querySelector(".cflogo")
+
+body.style.color = "blue"
+footer.style.color = "blue"
+footer.textContent = "error 404"
+
+
+
+/*body.innerHTML = "error 404"*/
+
+button.addEventListener('click', function(){
+    alert('Добрый день!')
+    h1.textContent = "Привет из JS!"
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let name1 = "Павел"
+let age = 15
+console.log("Меня зовут " + name1 + ". Мне " + age + " лет")*/
